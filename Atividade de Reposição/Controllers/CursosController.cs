993fb14040c6c3f8da35f03a1b3ae87b3a4a500c
Rafab <Rafab@DESKTOP-6MVMIL1>
2024-09-21 19:40:32 -0300

    Add project files.

---
+++ Atividade de Reposição/Controllers/CursosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Atividade_de_Reposição.Controllers
+{
+    public class CursosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ Atividade de Reposição/Models/Curso.cs
@@ -0,0 +1,11 @@
+﻿namespace Atividade_de_Reposição.Models
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+    }
+
+}
